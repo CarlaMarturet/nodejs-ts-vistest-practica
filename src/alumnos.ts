2455e63529120b7885277572dff708b68b18736c
Carla <abrevadero.4020@gmail.com>
2026-08-18 16:07:46 -0300

    creación de clase alumno + creacion de test alumno (passed)

---
+++ src/alumnos.ts
@@ -0,0 +1,12 @@
+export class Alumno {
+  constructor(
+    public nombre: string,
+    public edad: number
+  ) {}
+
+  esMayorDeEdad(): boolean {
+    return this.edad >= 18;
+  }
+}
+
+
---
+++ test/alumno.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "vitest";
+import { Alumno } from "../src/alumnos";
+
+describe("Alumno", () => {
+  test("un alumno de 18 años debe ser mayor de edad", () => {
+    const alumno = new Alumno("Juan", 18);
+
+    const resultado = alumno.esMayorDeEdad();
+
+    expect(resultado).toBe(true);
+  });
+});
